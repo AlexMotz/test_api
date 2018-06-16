9af13a544ba64f4189221b187716bbdf569a8ad9
Alex Motz <alexmotz@me.com>
2018-06-16 01:23:34 -0400

    Fixed time

--- routes/test.js
+++ routes/test.js
@@ -27,7 +27,7 @@ exports.ack_post = function(req, res, next) {
 exports.get_server_time = function(req, res, next) {
 	var time = Math.floor(Date.now() / 1000)
 	console.log("[GET] - get_server_time | Time: [" + time + "]");
-	res.sendStatus(time);
+	res.send(time);
 }
 
 
